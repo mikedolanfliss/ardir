7ef2b8f95cd8c79848ce4f9ed1897f7f53a9213c
Mike Dolan Fliss <mike.dolan.fliss@gmail.com>
2026-08-25 22:59:22 -0400

    Small fix attempts on the FARS BAC code

--- R/fars_api_make_table.R
+++ R/fars_api_make_table.R
@@ -16,6 +16,7 @@ fars_request_tbl = tibble(
   mutate(data = request |> map(\(x){read_csv(x, col_types = cols(.default = "c"))}))
 toc() # ~40 seconds for 2010-2024, 1 state; 10-15min for every state (state=*)
 # TODO SAVE, SHRINK, and REREAD! if this file is in data, don't redownload?
+# Could consider dropping in a .ignore folder.
 # fars_request_tbl |> saveRDS("data/fars_request_tbl.rds") # 86 megs, 1.3M rows x 162 variables
 
 # CREATE & RECODE PERSON TABLE ####
@@ -48,28 +49,35 @@ get_mvc_age_group = function(x){  # Helper function for age group recoding - ext
 fars_person_sm_tbl = fars_person_tbl |> 
   select(year = caseyear, state = statename, st_case, age, agename, sex = sexname, inj_kabco = inj_sevname, alc_bac_result = alc_res, alc_bac_text = alc_resname) # seems like what we need?
 # TODO Save here
-fars_person_sm_tbl |> saveRDS("data/fars_person_sm_tbl.rds")
+fars_person_sm_tbl |> saveRDS("data/fars_person_sm_tbl_orig.rds")
 
 fars_person_sm_tbl = fars_person_sm_tbl |> 
   filter(sex %in% c("Male", "Female"), !is.na(sex), age < 120, !is.na(age)) |> 
   mutate(sex = sex |> as_factor()) |> 
+  mutate(alc_bac_result = alc_bac_text |> str_extract("^[0-9\\.]+ ") |> as.numeric()) |> # Overwrite built in number variable
   mutate(alc_bac_result = case_when(
     alc_bac_text |> str_detect("% BAC|or Greater") ~ alc_bac_result, # Could also regex this, something like: "0.0[89]|0.[12345]"
     T ~  NA_integer_))  |> 
-  mutate(direct_alc_attributable = case_when( # ID >0.08 BAC CRASHES ####
-      alc_bac_result > 8 ~ TRUE, # "Directly Alcohol Attributable"
+  mutate(direct_alc_attributable = case_when( # ID >=0.08 BAC CRASHES ####
+      alc_bac_result >= 0.08 ~ TRUE, # "Directly Alcohol Attributable"
       T ~ FALSE)) # "Other / Mixed Cause"
 # TODO question - drop missing age -> missing age group?
 
-fars_person_sm_tbl |> count(alc_bac_result, alc_bac_text) |> arrange(desc(alc_bac_result)) # Could save and quarto a table like this. 
+# Hand check the BAC vars reveals issue: 0.84 and 0.084 both get "84" in number var.
+# fars_person_sm_tbl |> count(alc_bac_result, alc_bac_text) |> arrange(desc(alc_bac_result)) |> View() # Could save and quarto a table like this. 
 
-# Create person BAC % table ####
+# Create crash BAC % table ####
 fars_crashbac_tbl = fars_person_sm_tbl |>   
-  group_by(st_case) |> 
-  summarize(direct_alc_attributable = any(direct_alc_attributable))
+  group_by(year, state, st_case) |> 
+  summarize(crash_direct_alc_attributable = any(direct_alc_attributable))
+fars_person_sm_tbl = fars_person_sm_tbl |> 
+  left_join(fars_crashbac_tbl) # join was crash alcohol attributable (>0.08 BAC)
+# fars_person_sm_tbl |> filter(st_case |> str_detect("^1000[123]$")) |> arrange(st_case)
 
-fars_bac_pct_tbl = fars_person_sm_tbl |> 
-  left_join(fars_crashbac_tbl) |> # join was crash alcohol attributable (>0.08 BAC)
+fars_person_sm_tbl |> saveRDS("data/fars_person_sm_tbl.rds")
+
+# Calculate person AAF ####
+fars_bac_pct_tbl = fars_person_sm_tbl |>   
   filter(inj_kabco |> str_detect("(K)")) |> 
   mutate(age_group = age |> get_mvc_age_group()) |> 
   group_by(year, state, age_group, sex) |> 
@@ -88,10 +96,12 @@ fars_bac_pct_tbl = fars_bac_pct_tbl |>
   mutate(pct_alc_attributable = n_alc_att / total_n)
 fars_bac_pct_tbl
 fars_bac_pct_tbl |> saveRDS("data/fars_bac_pct_tbl.RDS")
+fars_bac_pct_tbl |> write_csv("data/fars_bac_pct_tbl.csv")
 # TODO will need to expand this for age (not age_group) joining. This table will be row_bound to the other documented ones. 
 # TODO calculate US-wide? Or just join into national data allowing state variation
 # TODO project today()'s yearly AAFs by duplicating prior year or a simple linear / loess type projection?
 
+# fars_person_sm_tbl |> filter(state == "North Carolina", inj_kabco |> str_detect("K")) |> arrange(age) |> View()
 # fars_bac_pct_tbl |> filter(state == "North Carolina") |> 
 #   ggplot(aes(x = year, y = pct_alc_attributable, color = age_group)) +
 #   facet_wrap(~sex)+
